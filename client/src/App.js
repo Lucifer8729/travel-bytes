bb1649e3f24cb46f23754a07524edc96a5d8f04f
Aniket Agarwal <66873475+Aniket0920@users.noreply.github.com>
2021-08-06 22:42:47 +0530

    Add route for Map

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Auth from "./components/Auth/Auth";
+import Map from "./components/Map/Map"
 
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import Auth from './components/Auth/Auth';
-
-const App = () => (
-  <BrowserRouter>
-    <Container maxWidth="lg">
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/auth" exact component={Auth} />
-      </Switch>
-    </Container>
-  </BrowserRouter>
-);
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Container maxidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/auth" exact component={Auth} />
+                    <Route path="/explore" exact component={Map} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
+    );
+}
 
 export default App;
