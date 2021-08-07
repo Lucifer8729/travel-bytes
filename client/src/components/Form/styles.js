07e184b23d1758e704dc48f1c9f6978693b54c7c
Aniket Agarwal <66873475+Aniket0920@users.noreply.github.com>
2021-08-08 01:51:13 +0530

    Changed postdetails,post.js

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -8,6 +8,7 @@ export default makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
+    border: '1px solid #064420'
   },
   form: {
     display: 'flex',
@@ -20,5 +21,29 @@ export default makeStyles((theme) => ({
   },
   buttonSubmit: {
     marginBottom: 10,
+    backgroundColor: '#064420',
+    color: '#e4efe7'
   },
+  buttonClear: {
+    backgroundColor: '#ffffff',
+    color: '#064420',
+    border: '2px solid #064420'
+  },
+  textField: {
+    "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#064420"
+    },
+    "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#064420"
+    },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#064420"
+    },
+    '& label.Mui-focused': {
+      color: '#064420',
+    },
+    title: {
+      color: 'solid #064420',
+    }
+  }
 }));
--- client/src/components/PostDetails/PostDetails.js
+++ client/src/components/PostDetails/PostDetails.js
@@ -29,11 +29,10 @@ const PostDetails = () => {
         <Paper style={{ padding: '20px', borderRadius: '15px' }} elevation={6}>
             <div className={classes.card}>
               <div className={classes.section}>
-                <Typography variant="h3" component="h2">{post.title}</Typography>
-                
-                <Typography gutterBottom variant="body1" component="p">{post.message}</Typography>
-                <Typography variant="h6">Created by: {post.name}</Typography>
-                <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
+                <Typography className={classes.username} variant="h6">Created by: {post.name}</Typography>
+                <Typography className={classes.createdAt} variant="body1">{moment(post.createdAt).fromNow()}</Typography>
+                <Typography className={classes.title} variant="h3" component="h2">{post.title}</Typography>
+                <Typography className={classes.body} gutterBottom variant="body1" component="p">{post.message}</Typography>
               </div>
               <div className={classes.imageSection}>
                 <img className={classes.media} src={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={post.title} />
